f7e863a486d015d3d135aef775fce278d2f27d32
tsvoboda-wake <taylorsmithfromm+wake@gmail.com>
2026-06-30 17:24:01 -0400

    feat: header, footer, css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,13 @@ export default function Footer() {
         >
           Visit my GitHub
         </a>
+        <a
+          href='https://www.linkedin.com/in/taylor-svoboda/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Let&apos;s connect on LinkedIn
+        </a>
       </p>
       <p>&copy; 2026 Taylor Svoboda. All rights reserved.</p>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,37 @@
-import logo from '../assets/ts-logo.png'
+import logo from '../assets/ts-logo.png';
 
 export default function Header() {
   return (
     <header className='header'>
-      <img src={logo} alt="Logo" className="logo" />
+      <div className='logoNamePos'>
+        <img src={logo} alt='Logo' className='logo' />
+        <div className='namePos'>
+          <p>Taylor Svoboda</p>
+          <p>Sr. Platform Engineer</p>
+        </div>
+      </div>
       <nav>
         <ul>
-          <li><a className='navItem' href="#about">About</a></li>
-          <li><a className='navItem' href="#projects">Projects</a></li>
-          <li><a className='navItem' href="#skills">Skills</a></li>
-          <li><a className='navItem' href="#experience">Experience</a></li>
+          <li>
+            <a className='navItem' href='#about'>
+              About
+            </a>
+          </li>
+          <li>
+            <a className='navItem' href='#projects'>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a className='navItem' href='#skills'>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a className='navItem' href='#experience'>
+              Experience
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
