5f9ba28f09b2614c59cfb7c38210aa995ffbeca5
tsvoboda-wake <taylorsmithfromm+wake@gmail.com>
2026-07-01 21:21:35 -0400

    fix: footer spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,8 @@ export default function Footer() {
         >
           Visit my GitHub
         </a>
+      </p>
+      <p>
         <a
           href='https://www.linkedin.com/in/taylor-svoboda/'
           target='_blank'
@@ -17,6 +19,7 @@ export default function Footer() {
           Let&apos;s connect on LinkedIn
         </a>
       </p>
+
       <p>&copy; 2026 Taylor Svoboda. All rights reserved.</p>
     </footer>
   );
